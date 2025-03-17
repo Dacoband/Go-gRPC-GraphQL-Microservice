7ceec28a2bec87d00e26c8199cb2387823eb626c
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2025-03-17 21:23:04 +0700

    go mod

---
+++ go-grpc-graphql-micro/graphql/mutation_resolver.go
@@ -0,0 +1,17 @@
+package main
+
+type mutationResolver struct {
+	server *Server
+}
+
+// func (r *mutationResolver) createAccount(ctx context.Context, in Account) (*Account, error) {
+
+// }
+
+// func (r *mutationResolver) createProduct(ctx context.Context, in Product) (*Product, error) {
+
+// }
+
+// func (r *mutationResolver) createOrder(ctx context.Context, in Order) (*Order, error) {
+
+// }
